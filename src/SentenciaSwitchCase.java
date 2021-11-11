a93a0a4a84ce9153c7889ded4ea24e3c9a5c66a6
Edwin Leon <e.leonardomatias@gmail.com>
2021-11-10 23:22:11 -0500

    version 11

--- src/SentenciaSwitchCase.java
+++ src/SentenciaSwitchCase.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class SentenciaSwitchCase {
     public static void main(String[] args) {
 
+    
         char num = 'p'; // soporta tambien: short, Integer, int, char, byte
 
         switch(num){
@@ -40,7 +43,61 @@ public class SentenciaSwitchCase {
             default: 
             System.out.println("Usuario desconocido");
         }
+
+
+//int mes = 12;
+
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Ingrese el numero del mes de 1-12");
+        int mes = s.nextInt();
+        String nombreMes = null;
+
+        switch(mes){
+
+            case 1:
+                nombreMes = "Enero";
+                break;
+            case 2:
+                nombreMes = "Febrero";
+                break;
+            case 3:
+                nombreMes = "Marzo";
+                break;
+            case 4:
+                nombreMes = "Abril";
+                break;
+            case 5:
+                nombreMes = "Mayo";
+                break;
+            case 6:
+                nombreMes = "Junio";
+                break;
+            case 7:
+                nombreMes = "Julio";
+                break;
+            case 8:
+                nombreMes = "Agosto";
+                break;
+            case 9:
+                nombreMes = "Septiembre";
+                break;
+            case 10:
+                nombreMes = "Octubre";
+                break;
+            case 11:
+                nombreMes = "Noviembre";
+                break;
+            case 12:
+                nombreMes = "Diciembre";
+                break;
+
+            default:
+            nombreMes = "Indefinido";
+            
+        }
         
+        System.out.println("nombreMes = " + nombreMes);
     }
     
 }
