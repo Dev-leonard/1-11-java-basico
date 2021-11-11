11cb15716b68e961bf7cfc3db4a77e43c018fdc3
Edwin Leon <e.leonardomatias@gmail.com>
2021-11-11 14:28:30 -0500

    version 12

---
+++ src/SentenciaSwitchCaseDiasMes.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class SentenciaSwitchCaseDiasMes {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Ingrese el numero del mes de 1 - 12");
+        int mes = s.nextInt();
+
+        int numeroDias = 0;
+        System.out.println("Ingrese el año (YYYY)");
+        int anio = s.nextInt();
+
+        switch (mes) {
+
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            numeroDias = 31;
+            break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            numeroDias = 31;
+            break;
+        case 2:
+            if (anio % 400 == 0 || ((anio % 4 == 0) && !(anio % 100 == 0))) {
+                ;
+                numeroDias = 29;
+            } else {
+                numeroDias = 28;
+            }
+            break;
+        default:
+            numeroDias = 0;
+        }
+        System.out.println("numeroDias = " + numeroDias);
+    }
+
+}
