97a59150a937e3d625523ffd5fdb9fc2c2355da0
Leon Matias <leon.matias.dev@gmail.com>
2021-11-16 16:31:49 -0500

    PropiedadesDeSistema

---
+++ src/EjemploPropiedadesDeSistema.java
@@ -0,0 +1,27 @@
+
+import java.util.Properties;
+
+public class EjemploPropiedadesDeSistema {
+    public static void main(String[] args) {
+
+        String username = System.getProperty("user.name");
+        System.out.println("username = " + username);
+
+        String home = System.getProperty("user.name");
+        System.out.println("home = " + home);
+
+        String workspace = System.getProperty("user.dir");
+        System.out.println("workspace = " + workspace);
+
+        String java = System.getProperty("java.version");
+        System.out.println("java = " + java);
+
+        String lineSeparator = System.getProperty("line.separator");
+        String lineSeparator2 = System.getProperty("line.separator");
+        System.out.println("lineSeparator:"  + lineSeparator2 + "una linea nueva");
+
+        Properties p = System.getProperties();
+        p.list(System.out);
+
+    }
+}
