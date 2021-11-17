f8fb10e8fb85ff948b9e17183a401b1d2da05e50
Leon Matias <leon.matias.dev@gmail.com>
2021-11-17 15:26:44 -0500

    ClaseMath2

--- src/EjemploClaseMath.java
+++ src/EjemploClaseMath.java
@@ -21,5 +21,36 @@ public class EjemploClaseMath {
 
         long entero = Math.round(Math.PI);
         System.out.println(" entero = " + entero);
+
+
+
+        double exp = Math.exp(2);
+        System.out.println(" exp = " + exp);
+
+         double log = Math.log(10);
+        System.out.println(" log = " + log);
+
+        double potencia = Math.pow(10, 3);
+        System.out.println(" potencia = " + potencia);
+
+        double raiz = Math.sqrt(5);
+        System.out.println(" raiz = " + raiz);
+
+        double grados = Math.toDegrees(1.57);
+        grados = Math.round(grados);  // redondea el decimal
+        System.out.println(" convertir de radianes a grados = " + grados);
+
+        double radianes = Math.toRadians(90.00);
+        System.out.println(" convertir de grados a radianes = " + radianes);
+
+        System.out.println(" sin(90): " + Math.sin(radianes));
+        System.out.println(" cos(90): " + Math.cos(radianes));
+
+        radianes = Math.toRadians(180.00);
+        System.out.println("cos(180): " + Math.cos(radianes));
+
+        radianes = Math.toRadians(0.00);
+        System.out.println("cos(0): " + Math.cos(radianes));
+
     }
 }
